5f328e65c2d4fbceca052328ad54662d8f2b819f
Ivan Khoda <ivankhoda@gmail.com>
2022-02-23 23:49:18 +0300

    Small refactories

--- client/src/components/PricesContainer/PricesContainer.tsx
+++ client/src/components/PricesContainer/PricesContainer.tsx
@@ -1,34 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { StockCard } from "../StockCard/StockCard";
-import { StockInfo } from "../StockInfoHelpers";
 import { StyledPricesContainer } from "./StyledPricesContainer";
+import { useFetch } from "./useFetch";
 
 export const PricesContainer = () => {
-  const [prices, setPrices] = useState<StockInfo[]>([]);
+  const { loading, data, error } = useFetch("http://localhost:8000/prices");
+  console.log(data, loading, error);
+  if (loading) return <h1>loading...</h1>;
+  if (error) return <h1>We have some troubles...</h1>;
 
-  useEffect(() => {
-    const fetchGoodsPrices = async () => {
-      const result = await fetch("http://localhost:8000/prices", {
-        method: "GET",
-        credentials: "same-origin",
-        headers: { "Content-Type": "application/json" },
-      });
-      setPrices(await result.json());
-    };
-    fetchGoodsPrices();
-  }, []);
-
-  return (
-    <StyledPricesContainer>
-      {prices.length !== 0 ? (
-        prices.map((item, i) => (
-          <StockCard key={i} name={item.name} currentPrice={item.currentPrice} previousPrice={item.previousPrice} />
-        ))
-      ) : (
-        <div>
-          <h1>Loading...</h1>
-        </div>
-      )}
-    </StyledPricesContainer>
-  );
+  // useEffect(() => {
+  //     const fetchGoodsPrices = async () => {
+  //       try {
+  //         const result = await fetch("http://localhost:8000/prices", {
+  //           method: "GET",
+  //           credentials: "same-origin",
+  //           headers: { "Content-Type": "application/json" },
+  //         });
+  //         setPrices(await result.json());
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     };
+  //     fetchGoodsPrices();
+  //   }, []);
+  if (data)
+    return (
+      <StyledPricesContainer>
+        {data.length !== 0 ? (
+          data.map((item, i) => (
+            <StockCard key={i} name={item.name} currentPrice={item.currentPrice} previousPrice={item.previousPrice} />
+          ))
+        ) : (
+          <div>
+            <h1>Loading...</h1>
+          </div>
+        )}
+      </StyledPricesContainer>
+    );
 };
--- client/src/components/PricesContainer/useFetch.ts
+++ client/src/components/PricesContainer/useFetch.ts
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
 export const useFetch = (uri: string) => {
   const [data, setData] = useState();
-  const [error, setError] = useState();
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     if (!uri) return;
-    fetch(uri)
-      .then((data) => data.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const result = await fetch(`${uri}`, {
+          method: "GET",
+          credentials: "same-origin",
+          headers: { "Content-Type": "application/json" },
+        });
+        setLoading(false);
+        setData(await result.json());
+      } catch (error) {
+        setError(true);
+      }
+    };
+
+    fetchData();
   }, [uri]);
+
   return {
     loading,
     data,
